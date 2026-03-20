9b7606c8ef4449cc62ea255a8f2c4b46dedf7822
a-ok123 <a-ok123@gmail.com>
2026-03-19 21:20:10 -0400

    Fixes on mac

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -2,10 +2,9 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     ft = { "markdown" },
-    build = "cd app && npm install",
+    build = "cd app && npm install && git checkout -- yarn.lock",
     config = function()
       vim.g.mkdp_auto_start = 0
     end,
   },
 }
-
